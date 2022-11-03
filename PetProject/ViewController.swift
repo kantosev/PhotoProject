97c69d043a726eea3c0cabfca43f0793f24c94a3
kantosev <103773083+kantosev@users.noreply.github.com>
2022-11-03 22:14:20 +0300

    Initial Commit

---
+++ PetProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetProject
+//
+//  Created by Антон Кирилюк on 03.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
