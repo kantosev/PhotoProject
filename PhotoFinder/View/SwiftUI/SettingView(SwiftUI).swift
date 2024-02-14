1f9e9e66b140886c45b997cb876f80bba0886ee8
Антон <103773083+kantosev@users.noreply.github.com>
2024-02-14 22:21:32 +0300

    Update SettingView

--- PhotoFinder/View/SwiftUI/SettingView(SwiftUI).swift
+++ PhotoFinder/View/SwiftUI/SettingView(SwiftUI).swift
@@ -22,19 +22,6 @@ struct SettingView: View {
     
     // Запись выбранного размера в UserDefaults
     init() {
-        UISegmentedControl.appearance().setAction(UIAction(handler: { _ in
-            UserDefaults.standard.set("1", forKey: "sizeImage")
-        }), forSegmentAt: 0)
-        UISegmentedControl.appearance().setAction(UIAction(handler: { _ in
-            UserDefaults.standard.set("2", forKey: "sizeImage")
-        }), forSegmentAt: 1)
-        UISegmentedControl.appearance().setAction(UIAction(handler: { _ in
-            UserDefaults.standard.set("3", forKey: "sizeImage")
-        }), forSegmentAt: 2)
-        UISegmentedControl.appearance().setAction(UIAction(handler: { _ in
-            UserDefaults.standard.set("4", forKey: "sizeImage")
-        }), forSegmentAt: 3)
-        
         // Окрас UISegmentedControl
         if #available(iOS 15.0, *) {
             UISegmentedControl.appearance().selectedSegmentTintColor = .systemIndigo
@@ -44,7 +31,7 @@ struct SettingView: View {
         
     }
     
-    @State var segmentationSelection : ProfileSection = (ProfileSection(rawValue: UserDefaults.standard.string(forKey: "sizeImage") ?? "3") ?? ProfileSection.med)
+    @AppStorage("sizeImage") var sizeImage: ProfileSection = ProfileSection.med
     
     var body: some View {
         
@@ -53,7 +40,7 @@ struct SettingView: View {
             Text("Quality of uploaded images")
                 .padding(EdgeInsets(top: 60, leading: 5, bottom: 0, trailing: 5))
             // Выбор размера изображения
-            Picker("", selection: $segmentationSelection) {
+            Picker("", selection: $sizeImage) {
                 ForEach(ProfileSection.allCases, id: \.self) { option in
                     Text(option.rawValue)
                 }
@@ -82,10 +69,8 @@ struct SettingView: View {
     
 }
 
-struct AboutView_SwiftUI__Previews: PreviewProvider {
-    static var previews: some View {
-        SettingView()
-    }
+#Preview {
+    SettingView()
 }
 
 
