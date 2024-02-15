38693ac8562b84352ddd23b5a5123897b2c00fcf
Антон <103773083+kantosev@users.noreply.github.com>
2024-02-15 20:27:16 +0300

    Done

---
+++ NetworkMonitorProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkMonitorProtocol.swift
+//  PhotoFinder
+//
+//  Created by Аркадий Варежкин on 15.02.2024.
+//
+
+import Foundation
+import Network
+
+
+protocol NetworkMonitorProtocol {
+    var isConnected: Bool { get }
+    var isExpensive: Bool { get }
+    func startMonitoring()
+    func stopMonitoring()
+    
+}
