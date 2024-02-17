a73e0cca2014bab1849031bab53e4e4d009f0b06
Антон <103773083+kantosev@users.noreply.github.com>
2024-02-17 19:26:34 +0300

    CollectionViewModel Test Done

---
+++ PhotoFinderTests/CollectionViewModelTests/CollectionViewModelTests.swift
@@ -0,0 +1,68 @@
+//
+//  CollectionViewModelTests.swift
+//  PhotoFinderTests
+//
+//  Created by Anton Kirilyuk on 17.02.2024.
+//
+
+import XCTest
+import Alamofire
+@testable import PhotoFinder
+
+final class CollectionViewModelTests: XCTestCase {
+    
+    /// Тестируемый объект - SUT
+    var sut: CollectionViewModel!
+    
+    /// Тестовый объект
+    var networkManagerStub: NetworkManagerProtocol!
+    
+    override func setUp() {
+        super.setUp()
+        networkManagerStub = NetworkManagerStub()
+        sut = CollectionViewModel(networkManager: networkManagerStub)
+    }
+    
+    override func tearDown() {
+        networkManagerStub = nil
+        sut = nil
+        super.tearDown()
+    }
+    
+    func testFetchOfData1() {
+        // arrange - подготовка (дано)
+        var boolChecked: Bool!
+        var errorFetchOfData: AFError?
+        
+        // act - выполнение тестируемого кода
+        sut.fetchOfData(with: "", searchButtonPressed: true) { arrayImagesIsEmpty in
+            boolChecked = arrayImagesIsEmpty
+        } errorCompletion: { error in
+            errorFetchOfData = error
+        }
+        
+        // assert - проверка
+        XCTAssertTrue(boolChecked)
+        XCTAssertNotNil(errorFetchOfData)
+    }
+    
+    func testFetchOfData2() {
+        // arrange - подготовка (дано)
+        var boolChecked: Bool!
+        var errorFetchOfData: AFError?
+        
+        // act - выполнение тестируемого кода
+        sut.fetchOfData(with: "", searchButtonPressed: false) { arrayImagesIsEmpty in
+            boolChecked = arrayImagesIsEmpty
+        } errorCompletion: { error in
+            errorFetchOfData = error
+        }
+        
+        // assert - проверка
+        XCTAssertTrue(boolChecked)
+        XCTAssertNotNil(errorFetchOfData)
+    }
+    
+    
+    
+}
